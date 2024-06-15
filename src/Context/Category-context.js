1ca85e1769e94de4051d47d99054a41f3b47e1f6
abhishek <shriabhishek929@gmail.com>
2024-06-16 00:47:27 +0530

    categories functionalities added

--- src/Context/Category-context.js
+++ src/Context/Category-context.js
@@ -1,18 +1,18 @@
 import React, { createContext, useState } from 'react';
 import { useContext } from 'react';
 
-const initialValue = {
-  hotelCategory: "National Parks"
-};
+const initialValue = "National Parks";
+
 
 export const CategoryContext = createContext(initialValue);
 
-export const CategoryProvider = ({ Children }) => {
-  const [state, setState] = useState(initialValue);
+export const CategoryProvider = ({ children }) => {
+  const [hotelCategory, setHotelCategory] = useState(initialValue);
+  
   
   return (
-    <CategoryContext.Provider value ={{ state, setState }}>
-      {Children}
+    <CategoryContext.Provider value ={{ hotelCategory, setHotelCategory }}>
+      {children}
     </CategoryContext.Provider>
   );
 };
